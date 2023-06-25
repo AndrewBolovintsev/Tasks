a4f69bddceeb9e5d3db66317852624956b1cdbdb
AndrewBolovintsev <basoo2512@gmail.com>
2023-06-25 14:50:34 +0300

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Andrew on 25.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
